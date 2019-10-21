47d57e5626700bb3a65ddd51e61bcac60b28a4ee
curtis <crutchc@yahoo.com>
2019-10-21 15:00:40 -0500

    corrected lines 43

--- src/Models/Facilities/ChickenHouse.cs
+++ src/Models/Facilities/ChickenHouse.cs
@@ -38,7 +38,7 @@ namespace Trestlebridge.Models.Facilities
             this._chickens.Add(chicken);
         }
 
-        public void AddResource(List<IGrazing> chickens)
+        public void AddResource(List<IFeed> chickens)
         {
 
             // TODO: implement this...
@@ -50,7 +50,7 @@ namespace Trestlebridge.Models.Facilities
             StringBuilder output = new StringBuilder();
             string shortId = $"{this._id.ToString().Substring(this._id.ToString().Length - 6)}";
 
-            output.Append($"Grazing field {shortId} has {this._chickens.Count} chickens\n");
+            output.Append($"Chicken house {shortId} has {this._chickens.Count} chickens\n");
             this._chickens.ForEach(a => output.Append($"   {a}\n"));
 
             return output.ToString();
