3385b8321c2ec62041bc62fac1aaf3ddebd81646
curtis <crutchc@yahoo.com>
2019-10-21 11:15:21 -0500

    testing logic

--- src/Actions/ChooseGrazingField.cs
+++ src/Actions/ChooseGrazingField.cs
@@ -4,25 +4,29 @@ using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
 
-namespace Trestlebridge.Actions {
-    public class ChooseGrazingField {
-        public static void CollectInput (Farm farm, IGrazing animal) {
+namespace Trestlebridge.Actions
+{
+    public class ChooseGrazingField
+    {
+        public static void CollectInput(Farm farm, IGrazing animal)
+        {
             Console.Clear();
 
             for (int i = 0; i < farm.GrazingFields.Count; i++)
             {
-                Console.WriteLine ($"{i + 1}. Grazing Field");
+                Console.WriteLine($"{i + 1}. Grazing Field");
             }
 
-            Console.WriteLine ();
+            Console.WriteLine();
 
             // How can I output the type of animal chosen here?
-            Console.WriteLine ($"Place the animal where?");
+            Console.WriteLine($"Place the animal where?");
 
-            Console.Write ("> ");
-            int choice = Int32.Parse(Console.ReadLine ());
+            Console.Write("> ");
+            int choice = Int32.Parse(Console.ReadLine());
+            int index = choice - 1;
 
-            farm.GrazingFields[choice].AddResource(animal);
+            farm.GrazingFields[index].AddResource(animal);
 
             /*
                 Couldn't get this to work. Can you?
